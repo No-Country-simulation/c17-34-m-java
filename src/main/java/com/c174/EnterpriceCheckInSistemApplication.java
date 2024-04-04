a92fea6fc99c80c54187c470eadca69075f9186f
Matias <akucfh@gmail.com>
2024-04-04 16:05:46 +0200

    check

--- src/main/java/com/c174/EnterpriceCheckInSistemApplication.java
+++ src/main/java/com/c174/EnterpriceCheckInSistemApplication.java
@@ -2,9 +2,8 @@ package com.c174;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class EnterpriceCheckInSistemApplication {
 
 	public static void main(String[] args) {
