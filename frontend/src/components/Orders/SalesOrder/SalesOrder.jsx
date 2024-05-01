c21a098854e35e4571d4ca374fab58d40ae461f1
aguslorca <aguslorca.12@gmail.com>
2024-05-01 18:02:10 -0300

    changes

--- frontend/src/components/Orders/SalesOrder/SalesOrder.jsx
+++ frontend/src/components/Orders/SalesOrder/SalesOrder.jsx
@@ -4,18 +4,22 @@ import { Link, NavLink } from "react-router-dom";
 import "../orders.css";
 import { MercadoPagoIcon } from "../../Icons/SocialMedia/MercadoPagoIcon";
 import PrimaryButton from "./../../Buttons/PrimaryButton/PrimaryButton";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import CompactTicketView from "../../Tickets/CompactTicketView/CompactTicketView";
-import Party2 from "../../../assets/images/Parties/party2.jpeg";
-import { AngleDownIcon } from "./../../Icons/Basic/AngleDownIcon";
 import { useAuth } from "../../Context/AuthProvider";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 const SalesOrder = () => {
+  const navigate = useNavigate();
   const auth = useAuth();
   const idUser = auth.user && auth.user.profile.id;
   const [isLoading, setIsLoading] = useState(true);
   const [tickets, setTickets] = useState([]);
+  const [selectedTicketId, setSelectedTicketId] = useState("");
+  const [selectedTicket, setSelectedTicket] = useState(null);
+  const [price, setPrice] = useState("");
+
+  console.log("selectedTicket", selectedTicket);
+
   const getTickets = async () => {
     try {
       const response = await axios.get(
@@ -33,6 +37,51 @@ const SalesOrder = () => {
     setIsLoading(true);
   }, [idUser]);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    const data = {
+      event: {
+        id: selectedTicket.event.id,
+        name: "",
+        dateStart: "2024-05-01T14:52:33.669Z",
+        dateEnd: "2024-05-01T14:52:33.669Z",
+        address: "",
+      },
+      owner: {
+        id: idUser,
+      },
+      price: price,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://troca-prod-main.onrender.com/ticket/create/fast",
+        data
+      );
+      if (response.status === 201) {
+        navigate("/tickets/purchase");
+        console.log("Orden de venta creada exitosamente.");
+      } else {
+        console.error("Error al crear la orden de venta.");
+      }
+    } catch (error) {
+      console.error("Ocurrió un error inesperado.", error);
+    }
+  };
+
+  const handleTicketChange = (event) => {
+    const selectedId = event.target.value;
+    console.log("selectedId", selectedId);
+  
+    setSelectedTicketId(selectedId);
+    const ticket = tickets.find((ticket) => ticket.id === selectedId);
+    setSelectedTicket(ticket);
+  };
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+  
   return (
     <>
       <TopBarMobile title="Crea una oferta" linkTo="/offers/active" />
@@ -53,19 +102,18 @@ const SalesOrder = () => {
               </li>
             </ul>
             <h2>Detalles de su orden</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               <label htmlFor="eventName">Entrada</label>
-              <select className="accordion-content">
-                <option value="selected">
-                  {" "}
-                  Seleccione un ticket de su wallet
-                </option>
+              <select
+                className="accordion-content"
+                onChange={handleTicketChange}
+              >
+                <option value="">Seleccione un ticket de su wallet</option>
                 {tickets.map((ticket) => (
                   <option key={ticket.id} value={ticket.id}>
-                    {ticket.event.name} - {ticket.eventDate}
+                    {ticket.event.name}
                   </option>
                 ))}
-                
               </select>
               <label htmlFor="price">Precio</label>
               <input
@@ -73,6 +121,8 @@ const SalesOrder = () => {
                 id="price"
                 name="price"
                 placeholder="ARS$ 0"
+                value={price}
+                onChange={handlePriceChange}
               />
               <div className="payment-method">
                 <h3>Método de cobro</h3>
@@ -86,14 +136,14 @@ const SalesOrder = () => {
                 <p>
                   Al crear una orden de venta, su entrada será retenida por la
                   plataforma para proporcionar una experiencia de compra y venta
-                  mas segura y rápida. En caso de querer cancelar su orden de
+                  más segura y rápida. En caso de querer cancelar su orden de
                   venta, puede administrarlo desde el apartado “Mis ordenes” y
                   eliminar la orden de venta. Su entrada estará disponible en su
                   wallet.
                 </p>
               </div>
               <PrimaryButton
-                type={"submit"}
+                type="submit"
                 backColor="var(--color-red)"
                 text="Crear orden de venta"
               />
